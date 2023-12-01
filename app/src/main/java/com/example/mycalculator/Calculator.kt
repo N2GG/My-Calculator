8c3a53d2b587deee1ff0a67ae42dfa0b504c731f
N2GG <nol2ggoon2@gmail.com>
2023-12-01 10:39:26 +0900

    나머지 구하기 추가

--- app/src/main/java/com/example/mycalculator/Calculator.kt
+++ app/src/main/java/com/example/mycalculator/Calculator.kt
@@ -3,7 +3,7 @@ package com.example.mycalculator
 fun main() {
     println("계산기에 오신것을 환영합니다")
     println("무엇을 도와드릴까요?")
-    println("[1] 더하기 [2] 빼기 [3] 곱하기 [4] 나누기 [5] 종료")
+    println("[1] 더하기 [2] 빼기 [3] 곱하기 [4] 나누기 [5] 나머지 [6] 종료")
 
     var menu1 = readln().toInt()
 
@@ -83,6 +83,23 @@ fun main() {
     }
 
     else if (menu1 == 5) {
+        println("나머지를 구하고 싶은 숫자 두개를 입력하세요")
+        var num1 = readln().toInt()
+        var num2 = readln().toInt()
+        var quotient = num1 % num2
+
+        println("나머지를 구한 결과는 $quotient 입니다.")
+        println("추가적인 작업이 필요하신가요?")
+        println("[1] 네 [2] 아니오.")
+
+        var extraMenu = readln().toInt()
+        if (extraMenu == 1) {
+            println("초기화면으로 돌아갑니다.")
+            return main()
+        } else println("이용해주셔서 감사합니다. 안녕히 가세요.")
+    }
+
+    else if (menu1 == 6) {
         println("이용해주셔서 감사합니다. 안녕히 가세요.")
         return
     }
